51b875c6774dbdd5534bead9486fab8d2a85453b
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-04-10 08:11:20 +0700

    Adding Status Bar Feature-FrontEnd with Dummy Data in List Front

--- instaclone-front-web/src/Components/StatusBar/StatusBar.js
+++ instaclone-front-web/src/Components/StatusBar/StatusBar.js
@@ -1,10 +1,55 @@
+import { Avatar } from '@material-ui/core';
 import React, { Component } from 'react';
 import './StatusBar.css';
+import statusimg from "../../images/pp1.png";
+
 export default class StatusBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            statusList: []
+        }
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = () => {
+        let data = [
+            {
+                "username": "irfaanhibatullah",
+                "imageURL": "../../images/pp1.png"
+            },
+            {
+                "username": "ahmadirfaan",
+                "imageURL": "../../images/pp1.png"
+            },
+            {
+                "username": "tutiwulandari",
+                "imageURL": "../../images/pp1.png"
+            },
+            {
+                "username": "atmimnurrona",
+                "imageURL": "../../images/pp1.png"
+            }
+        ]
+        this.setState({ statusList: data });
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="statusbar_container">
+                {
+                    this.state.statusList.map((item, index) => (
+                        <div className="status">
+                            <Avatar className="statusbar_status" src={statusimg} />
+                            <div className="statusbar_text">{item.username}</div>
+                        </div>
+                    ))
+                }
+
             </div>
         )
     }
