e2e9e832e136cc4bb97a32fb709a4e2d34298d36
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-04-11 20:27:31 +0700

    Making Post Section with Dummy DatA

--- instaclone-front-web/src/Components/MainPage/MainPage.js
+++ instaclone-front-web/src/Components/MainPage/MainPage.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
+import Post from '../Post/Post';
 import './MainPage.css';
 
 export default class MainPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            postArray: []
+        }
     }
+
+    componentDidMount() {
+        this.getPost()
+    }
+
+    getPost = () => { //Untuk API Dummy
+        let data = [
+            {
+                "postId": "123456",
+                "userName": "andhika_harris",
+                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+                "timestamp": "12345",
+                "likes": "1234"
+            },
+            {
+                "postId": "123456",
+                "userName": "andhika_harris",
+                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+                "timestamp": "12345",
+                "likes": "1234"
+            },
+            {
+                "postId": "123456",
+                "userName": "andhika_harris",
+                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+                "timestamp": "12345",
+                "likes": "1234"
+            },
+            {
+                "postId": "123456",
+                "userName": "andhika_harris",
+                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+                "timestamp": "12345",
+                "likes": "1234"
+            }
+        ];
+        this.setState({ postArray: data })
+
+    }
+
     render() {
         return (
             <div>
-                
+                {
+                    this.state.postArray.map((item, index) => (
+                        <Post id={item.postId} userName={item.userName} 
+                        postImage={item.postImageURL}
+                        likes={item.likes}
+                        />
+
+                    ))
+
+                }
             </div>
         )
     }
---
+++ instaclone-front-web/src/Components/Post/Post.js
@@ -0,0 +1,83 @@
+import { Avatar } from '@material-ui/core';
+import React, { Component } from 'react';
+import './Post.css';
+import postimage from "../../images/post.jpg";
+import love from "../../images/love.svg";
+import comment from "../../images/comment.svg";
+import share from "../../images/share.svg";
+
+class Post extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            commentList: []
+        }
+    }
+
+    componentDidMount() {
+        this.getComments()
+    }
+
+    getComments = () => { //Contoh Dummy Data sebelum back-end
+        let data = [
+            {
+                "username": "ASD",
+                "commentId": "1234",
+                "timeStamp": "123456",
+                "description": "Comment 1"
+            },
+            {
+                "username": "ASD",
+                "commentId": "1234",
+                "timeStamp": "123456",
+                "description": "Comment 2"
+            },
+            {
+                "username": "ASD",
+                "commentId": "1234",
+                "timeStamp": "123456",
+                "description": "Comment 3"
+            }
+        ];
+        this.setState({ commentList: data });
+    }
+    render() {
+        return (
+            <div className="post_container">
+                {/* Merepresentasikan Header(Tampilan profil,dkk) */}
+                <div className="post_header">
+                    <Avatar className="post_image" src="" />
+                    <div className="post_username" >{this.props.useNname}</div>
+                </div>
+
+                {/* Merepresentasikan Gambar yang dipos */}
+                <div>
+                    <img src={this.props.postImage} width="615px" />
+                </div>
+
+                {/* Merepresentasikan jumlah like, data analitik */}
+                <div>
+                    <div style={{ marginLeft: "10px" }}>
+                        <img src={love} className="post_reactimage"></img>
+                        <img src={comment} className="post_reactimage"></img>
+                        <img src={share} className="post_reactimage"></img>
+                    </div>
+                    <div style={{ fontWeight: "bold", marginLeft: "20px" }}>
+                        {this.props.likes} likes
+                    </div>
+                </div>
+                {/* Merepresentasikan komen,dll */}
+                <div>
+                    {
+                        this.state.commentList.map((item, index) => (
+                            <div className="post_comment">{item.username} : {item.description}</div>
+                        ))
+                    }
+                    <input text="text" className="post_commentbox" placeholder="Add a comment..." />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Post;
